a57b32b9c1593a8258c4f2ff74df2f8aac070f15
yvesmuniz <48829706+yvesmuniz@users.noreply.github.com>
2020-06-04 12:50:42 -0300

    Update ex1.cpp

--- revisao/ex1.cpp
+++ revisao/ex1.cpp
@@ -5,30 +5,25 @@
 int maiorN(int a, int *maiorNumero);
 
 int main (void){
-	
 	setlocale(LC_ALL,"Portuguese");
 	
 	int maiorNumero;
 	
 	maiorN(20,&maiorNumero); 
 
-	printf("\nO maior número é: %i", maiorNumero);
-	
-		
+	printf("\nO maior nÃºmero Ã©: %i", maiorNumero);	
 	return 0;
 }
 
 int maiorN(int a, int *maiorNumero=0){
 	int numeros[a];
-	
-	for(int i=0;i<a;i++){
-		printf("Digite o %iº numero: \n", i+1);
+	for(int i=0; i < a; i++){
+		printf("Digite o %iÂº numero: \n", i+1);
 		scanf("%i",&numeros[i]);
 		if (numeros[i]>*maiorNumero){
 			*maiorNumero = numeros[i];
 		}
 	}
-	return *maiorNumero;
-	
+	return *maiorNumero;	
 }
 
