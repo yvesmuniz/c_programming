64b4d7324b2226e6c8987b79b21340ece07d7df0
Yves Muniz <yvesmuniz@gmail.com>
2020-06-04 12:42:43 -0300

    Adicionado exercício 01

---
+++ revisao/ex1.cpp
@@ -0,0 +1,34 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include"locale.h"]
+
+int maiorN(int a, int *maiorNumero);
+
+int main (void){
+	
+	setlocale(LC_ALL,"Portuguese");
+	
+	int maiorNumero;
+	
+	maiorN(20,&maiorNumero); 
+
+	printf("\nO maior número é: %i", maiorNumero);
+	
+		
+	return 0;
+}
+
+int maiorN(int a, int *maiorNumero=0){
+	int numeros[a];
+	
+	for(int i=0;i<a;i++){
+		printf("Digite o %iº numero: \n", i+1);
+		scanf("%i",&numeros[i]);
+		if (numeros[i]>*maiorNumero){
+			*maiorNumero = numeros[i];
+		}
+	}
+	return *maiorNumero;
+	
+}
+
